51d063cb7c56c06b407e9ff9d8c590df9f9e9c0d
ofer <ofergavri@gmail.com>
2023-02-20 20:04:10 +0200

    update build

--- src/app/cmps/contact-list/contact-list.component.ts
+++ src/app/cmps/contact-list/contact-list.component.ts
@@ -3,6 +3,7 @@ import { Contact } from 'src/app/models/contact.model';
 import { faUserPen } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'contact-list',
@@ -10,9 +11,19 @@ import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./contact-list.component.scss']
 })
 export class ContactListComponent {
+
+  constructor(private contactService: ContactService) { }
   @Input() contacts!: Contact[] | null;
 
   faUserPen = faUserPen
   faTrash = faTrash
   faUserPlus = faUserPlus
+
+  onRemove(contactId: string | undefined) {
+    try {
+      this.contactService.deleteContact(contactId as string)
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
