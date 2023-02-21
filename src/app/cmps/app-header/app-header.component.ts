2418969f7431f924585a5c274a518b89a047e953
Idan David <idand2000@gmail.com>
2023-02-21 16:45:39 +0200

    fix header bug

--- src/app/cmps/app-header/app-header.component.ts
+++ src/app/cmps/app-header/app-header.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
@@ -7,11 +9,23 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss']
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit, OnDestroy  {
   user!: User
+  subscription!: Subscription
 
-  constructor(private userService: UserService){
-    this.user = this.userService.getUser()
+  constructor(
+    private userService: UserService,
+    private router: Router){
+      this.user = this.userService.getUser()
   }
 
+  ngOnInit() {
+    this.subscription = this.router.events.subscribe(() => {
+      this.user = this.userService.getUser()
+    })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
+  }
 }
